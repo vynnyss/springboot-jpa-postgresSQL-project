f65cd38c3c4d89263f816978a49b6cd5339d653c
Vinicius Sousa <vinissousa016@gmail.com>
2025-07-08 18:08:07 -0300

    OrderStatus enum

--- src/main/java/com/example/course/config/TestConfig.java
+++ src/main/java/com/example/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.example.course.entities.Order;
 import com.example.course.entities.User;
+import com.example.course.entities.enums.OrderStatus;
 import com.example.course.repositories.OrderRepository;
 import com.example.course.repositories.UserRepository;
 
@@ -32,11 +33,11 @@ public class TestConfig implements CommandLineRunner {
 		User u3 = new User(null, "Corin", "corin@ZZZ.com", "977777776", "1234567");
 		User u4 = new User(null, "Miyabi@ZZZ.com", "Miyabi", "123123","131241241");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1); 
-		Order o4 = new Order(null, Instant.parse("2019-07-20T05:42:10Z"), u3);
-		Order o5 = new Order(null, Instant.parse("2019-07-23T10:21:22Z"), u4); 
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAAYMENT, u1); 
+		Order o4 = new Order(null, Instant.parse("2019-07-20T05:42:10Z"), OrderStatus.SHIPPED, u3);
+		Order o5 = new Order(null, Instant.parse("2019-07-23T10:21:22Z"), OrderStatus.DELIVERED,u4); 
 		
 		userRepository.saveAll(Arrays.asList(u1, u2, u3, u4));	
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3, o4, o5));
