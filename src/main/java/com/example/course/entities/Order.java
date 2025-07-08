620f8f0bf6ce2bb319ab342da9475d010b06bcf2
Vinicius Sousa <vinissousa016@gmail.com>
2025-07-08 18:50:12 -0300

    added Categories table

--- src/main/java/com/example/course/entities/Order.java
+++ src/main/java/com/example/course/entities/Order.java
@@ -23,12 +23,11 @@ public class Order implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
 	private Integer orderStatus;
-
+	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
